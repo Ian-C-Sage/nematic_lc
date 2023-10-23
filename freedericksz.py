08a958c23a48ff2d447ab26e1444e46569551b1b
Ian <ian@sage.myzen.co.uk>
2023-10-23 12:10:30 +0100

    Correction to routines reflecting correct definition of reference axis.

--- freedericksz.py
+++ freedericksz.py
@@ -8,21 +8,10 @@ import matplotlib.pyplot as plt
 from scipy.optimize import root
 
 eps0=8.854e-12
-cell_thickness=10e-6
-voltage=30.0
-grid_points=51
-k_11=1e-10 # LC elastic constant
-k_33=2e-10 # LC elastic constant
-eps_par=15.0
-eps_perp=5.0
-gamma=10.0 # LC viscosity
-sim_length=10.0 # Units of time
-delta_t=0.0005 # Time increment
-record_every=2500 # How many time increments between plots
-
-z_vals=np.linspace(0, cell_thickness, grid_points) # grid_points values
-tilt1=np.pi/18000.0
-tilt2=np.pi/18000.0
+#gamma=10.0 # LC viscosity
+#sim_length=10.0 # Units of time
+#delta_t=0.0005 # Time increment
+#record_every=2500 # How many time increments between plots
 
 # Return a smoothly varying tilt profile at the z values specified in z_vals, matching boundary
 # conditions at z=0 and z=cell_thickness, and with a maximum deviation from straight line
@@ -35,7 +24,6 @@ def initialise_theta(z_vals, cell_thickness, tilt1, tilt2, max_tilt):
     thetas=np.array([tilt1+(tilt2-tilt1)*(zval/cell_thickness)+max_tilt*sin(pi*(zval/cell_thickness)) for zval in z_vals])
     return(thetas)
                  
-a=initialise_theta(z_vals, cell_thickness, tilt1, tilt2, np.pi/2.0)
 
 
 # Given the LC properties and tilt profile, calculate the non-uniform
@@ -85,21 +73,23 @@ def voltage_distribution2(d, theta_vals, voltage, eps_par, eps_perp):
 
 # (ε_par-ε_perp)*('diff(V,z,1))^2*ε_0*cos(θ)*sin(θ)+(K*(θ[i+1]-2*θ[i]+θ[i-1]))/h^2='diff(θ,t,1).γ
 def euler_lagrange(theta_vals, cell_thickness, k_33, k_11, eps_par, eps_perp, voltage):
-    num_eqs=len(theta_vals)-2
     h=cell_thickness/(len(theta_vals)-1)
-    theta_plus=theta_vals[2:]
-    theta_minus=theta_vals[:-2]
-    theta_mid=theta_vals[1:-1]
+    # We'll be using a finite difference approximation to the derivative terms dtheta/dz and
+    # d^2theta/dz^2. The equations we need to solve are centred on theta[1] to theta[-2];
+    # theta[0] and theta[-1] are constant boundary values. Approximate the derivatives by central
+    # differences, and to aid this, define the theta ranges centred on i+1 and i-1.
+    theta_plus=np.array(theta_vals[2:])
+    theta_minus=np.array(theta_vals[:-2])
+    theta_mid=np.array(theta_vals[1:-1])
     dtheta_dz=(theta_plus-theta_minus)/(2*h)
     d2theta_dz2=(theta_plus+theta_minus-2*theta_mid)/(h*h)
     ct=np.cos(theta_mid)
     ct2=ct*ct
     st=np.sin(theta_mid)
-    term1=((k_33-k_11)*ct2+k_11)*d2theta_dz2
-    term2=(k_11-k_33)*st*ct*dtheta_dz*dtheta_dz
+    term1=((k_11-k_33)*ct2+k_33)*d2theta_dz2
+    term2=(k_33-k_11)*st*ct*dtheta_dz*dtheta_dz
     vd=voltage_distribution2(cell_thickness, theta_vals, voltage, eps_par, eps_perp)
     field=np.array([(vd[i]+vd[i+1])/(2.0*h) for i in range(len(vd)-1)])
-
     term3=(eps_par-eps_perp)*eps0*field*field*ct*st
     el=term1+term2+term3
     return el
@@ -107,32 +97,42 @@ def euler_lagrange(theta_vals, cell_thickness, k_33, k_11, eps_par, eps_perp, vo
 def el_wrapper(theta_cen, params):
     theta_vals=np.array([0 for i in range(len(theta_cen)+2)], float)
     theta_vals[1:-1]=theta_cen
-    theta_vals[0]=tilt1
-    theta_vals[-1]=tilt2
+    #theta_vals[0]=tilt1
+    #theta_vals[-1]=tilt2
+    theta_vals[0]=params[0]
+    theta_vals[-1]=params[1]
     el=euler_lagrange(theta_vals, params[2], params[3], params[4], params[5], params[6], params[7])
     return el
 
-#v_range=[20, 15, 10, 9, 8, 7, 6, 5, 4, 3.5, 3]
-v_range=[5.0, 4.5, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0]
-profile=np.zeros((grid_points, len(v_range)), float)
-profile[0, :]=tilt1
-profile[grid_points-1, :]=tilt2
-for v_index, voltage in enumerate(v_range):
-    b=root(el_wrapper, a[1:-1], [tilt1, tilt2, cell_thickness, k_33, k_11, eps_par, eps_perp, voltage], method='hybr', options={'factor':0.1})
-    for t_index, theta_val in enumerate(b.x):
-        profile[t_index+1, v_index]=theta_val
-        a[t_index+1]=theta_val
-"""
-v=voltage_distribution(cell_thickness, a, 1.0, 10, 5)
-v2=voltage_distribution2(cell_thickness, a, 1.0, 10, 5)
-"""
-fig, ax = plt.subplots()
-c=b.x
-c=np.insert(c, 0, tilt1)
-c=np.append(c, tilt2)
-for index in range(len(v_range)):
-    ax.plot(z_vals, profile[:, index], label=str(v_range[index])+'V')
-ax.legend()
-plt.show()
+def run_it():
+    cell_thickness=10e-6
+    grid_points=21
+    k_11=2e-10 # LC elastic constant
+    k_33=1e-10 # LC elastic constant
+    eps_par=15.0
+    eps_perp=5.0
+    z_vals=np.linspace(0, cell_thickness, grid_points) # grid_points values
+    tilt1=0.0
+    tilt2=0.0
+    a=initialise_theta(z_vals, cell_thickness, tilt1, tilt2, np.pi/4.0)
+    #v_range=[20, 15, 10, 9, 8, 7, 6, 5, 4, 3.5, 3]
+    v_range=[5.0, 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3]
+    profile=np.zeros((grid_points, len(v_range)), float)
+    profile[0, :]=tilt1
+    profile[grid_points-1, :]=tilt2
+    for v_index, voltage in enumerate(v_range):
+        b=root(el_wrapper, a[1:-1], [tilt1, tilt2, cell_thickness, k_33, k_11, eps_par, eps_perp, voltage], method='hybr', options={'factor':0.1})
+        for t_index, theta_val in enumerate(b.x):
+            profile[t_index+1, v_index]=theta_val
+            a[t_index+1]=theta_val
+    fig, ax = plt.subplots()
+    c=b.x
+    c=np.insert(c, 0, tilt1)
+    c=np.append(c, tilt2)
+    for index in range(len(v_range)):
+        ax.plot(z_vals, profile[:, index], label=str(v_range[index])+'V')
+    ax.legend()
+    plt.show()
 
-
+if __name__=='__main__':
+    run_it()
