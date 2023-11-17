aab2c15b62d53aa2f4546cb7f6213e33cb0ac4bc
Ian <ian@sage.myzen.co.uk>
2023-11-17 15:55:59 +0000

    Add twisted nematic routine and ancillary files

--- freedericksz.py
+++ freedericksz.py
@@ -1,79 +1,69 @@
-# Solve the continuum equation set, describing the relaxation of a twist-free
-# nematic layer from an initially distorted configuration to a relaxed state in
-# the presence of an applied field.
+"""Solve the continuum equation set, describing the relaxation of a twist-free
+nematic layer from an initially distorted configuration to a relaxed state in
+the presence of an applied field."""
 
+from math import pi, sin
 import numpy as np
-from math import pi, sin, log
 import matplotlib.pyplot as plt
 from scipy.optimize import root
 
 eps0=8.854e-12
-#gamma=10.0 # LC viscosity
-#sim_length=10.0 # Units of time
-#delta_t=0.0005 # Time increment
-#record_every=2500 # How many time increments between plots
 
-# Return a smoothly varying tilt profile at the z values specified in z_vals, matching boundary
-# conditions at z=0 and z=cell_thickness, and with a maximum deviation from straight line
-# interpolation of max_tilt
-#
-# The routine is used to seed a smoothly tilted director configuration as a starting point for
-# the numerical solution routines, to avoid the trivial solution tilt=0 (for all z) which is
-# valid at all applied fields if tilt1=-tilt2
 def initialise_theta(z_vals, cell_thickness, tilt1, tilt2, max_tilt):
+    """The routine is used to seed a smoothly tilted director configuration as a starting point for
+    the numerical solution routines, to avoid the trivial solution tilt=0 (for all z) which is
+    valid at all applied fields if tilt1=-tilt2"""
     thetas=np.array([tilt1+(tilt2-tilt1)*(zval/cell_thickness)+max_tilt*sin(pi*(zval/cell_thickness)) for zval in z_vals])
-    return(thetas)
-                 
+    return thetas
 
+def voltage_distribution(cell_thickness, theta_vals, voltage, eps_par, eps_perp):
+    """Given the LC properties and tilt profile, calculate the non-uniform
+    potential distribution across a nematic layer resulting from the LC
+    anisotropy. Interpolate theta values in gaps."""
 
-# Given the LC properties and tilt profile, calculate the non-uniform
-# potential distribution across a nematic layer resulting from the LC
-# anisotropy. Interpolate theta values in gaps.
-def voltage_distribution(d, theta_vals, voltage, eps_par, eps_perp):
     # For N grid points, there are N-1 dielectric layers. Interpolate the tilt angles.
     gap_thetas=[(theta_vals[i]+theta_vals[i+1])/2.0 for i in range(len(theta_vals)-1)]
-    ct=np.cos(gap_thetas) # cos(theta)
-    ct_sq=ct*ct # cos(theta)^2
-    eps=eps_perp*ct_sq+eps_par*(1-ct_sq) # Effective relative permittivity for a tilted sub-layer
-    cap=eps0*eps/(d/(len(theta_vals)-1)) # Capacitance of each sub-layer, per unit area
+    cos_theta=np.cos(gap_thetas) # cos(theta)
+    cos_theta_sq=cos_theta*cos_theta # cos(theta)^2
+    eps=eps_perp*cos_theta_sq+eps_par*(1-cos_theta_sq) # Effective relative permittivity for a tilted sub-layer
+    cap=eps0*eps/(cell_thickness/(len(theta_vals)-1)) # Capacitance of each sub-layer, per unit area
     total_cap=1/np.sum(1/cap) # Capacitance of the whole stack of layers, in series
-    Q=voltage*total_cap # Charge on the stack, per unit area, under potential of voltage
-    potential_drop=Q/cap # voltage drop across each sub-layer
+    charge_density=voltage*total_cap # Charge on the stack, per unit area, under potential of voltage
+    potential_drop=charge_density/cap # voltage drop across each sub-layer
     potential=np.array(theta_vals)
     potential[0]=0.0
     for i in range(len(potential_drop)):
         potential[i+1]=potential[i]+potential_drop[i]
     potential[-1]=voltage
-    #potential_drop[0]=potential_drop[0]/2.0
-    #potential_drop[-1]=potential_drop[-1]/2.0
     return potential_drop
 
-# Given the LC properties and tilt profile, calculate the non-uniform
-# potential distribution across a nematic layer resulting from the LC
-# anisotropy. Interpolate epsilon values in gaps.
-def voltage_distribution2(d, theta_vals, voltage, eps_par, eps_perp):
+def voltage_distribution2(cell_thickness, theta_vals, voltage, eps_par, eps_perp):
+    """Given the LC properties and tilt profile, calculate the non-uniform
+    potential distribution across a nematic layer resulting from the LC
+    anisotropy. Interpolate epsilon values in gaps."""
+
     # For N grid points, there are N-1 dielectric layers. Interpolate the permittivities.
     #gap_thetas=[(theta_vals[i]+theta_vals[i+1])/2.0 for i in range(len(theta_vals)-1)]
-    ct=np.cos(theta_vals) # cos(theta)
-    ct_sq=ct*ct # cos(theta)^2
-    eps=eps_perp*ct_sq+eps_par*(1-ct_sq) # Effective relative permittivity at each grid point
+    cos_theta=np.cos(theta_vals) # cos(theta)
+    cos_theta_sq=cos_theta*cos_theta # cos(theta)^2
+    eps=eps_perp*cos_theta_sq+eps_par*(1-cos_theta_sq) # Effective relative permittivity at each grid point
     gap_eps=np.array([(eps[i]+eps[i+1])/2.0 for i in range(len(theta_vals)-1)])
-    cap=eps0*gap_eps/(d/(len(theta_vals)-1)) # Capacitance of each sub-layer, per unit area
+    cap=eps0*gap_eps/(cell_thickness/(len(theta_vals)-1)) # Capacitance of each sub-layer, per unit area
     total_cap=1/np.sum(1/cap) # Capacitance of the whole stack of layers, in series
-    Q=voltage*total_cap # Charge on the stack, per unit area, under potential of voltage
-    potential_drop=Q/cap # voltage drop across each sub-layer
+    charge_density=voltage*total_cap # Charge on the stack, per unit area, under potential of voltage
+    potential_drop=charge_density/cap # voltage drop across each sub-layer
     potential=np.array(theta_vals)
     potential[0]=0.0
     for i in range(len(potential_drop)):
         potential[i+1]=potential[i]+potential_drop[i]
     potential[-1]=voltage
-    #potential_drop[0]=potential_drop[0]/2.0
-    #potential_drop[-1]=potential_drop[-1]/2.0
     return potential_drop
 
-# (ε_par-ε_perp)*('diff(V,z,1))^2*ε_0*cos(θ)*sin(θ)+(K*(θ[i+1]-2*θ[i]+θ[i-1]))/h^2='diff(θ,t,1).γ
 def euler_lagrange(theta_vals, cell_thickness, k_33, k_11, eps_par, eps_perp, voltage):
-    h=cell_thickness/(len(theta_vals)-1)
+    """ The Euler-Lagrange equation is defined in the .wxmx file. This routine takes a vector
+    argument of length grid_points and returns a vector or length grid_points-2; the wrapper
+    function takes care of this and also handles the constant arguments correctly"""
+    grid_spacing=cell_thickness/(len(theta_vals)-1)
     # We'll be using a finite difference approximation to the derivative terms dtheta/dz and
     # d^2theta/dz^2. The equations we need to solve are centred on theta[1] to theta[-2];
     # theta[0] and theta[-1] are constant boundary values. Approximate the derivatives by central
@@ -81,58 +71,66 @@ def euler_lagrange(theta_vals, cell_thickness, k_33, k_11, eps_par, eps_perp, vo
     theta_plus=np.array(theta_vals[2:])
     theta_minus=np.array(theta_vals[:-2])
     theta_mid=np.array(theta_vals[1:-1])
-    dtheta_dz=(theta_plus-theta_minus)/(2*h)
-    d2theta_dz2=(theta_plus+theta_minus-2*theta_mid)/(h*h)
-    ct=np.cos(theta_mid)
-    ct2=ct*ct
-    st=np.sin(theta_mid)
-    term1=((k_11-k_33)*ct2+k_33)*d2theta_dz2
-    term2=(k_33-k_11)*st*ct*dtheta_dz*dtheta_dz
+    dtheta_dz=(theta_plus-theta_minus)/(2*grid_spacing)
+    d2theta_dz2=(theta_plus+theta_minus-2*theta_mid)/(grid_spacing*grid_spacing)
+    cos_theta=np.cos(theta_mid)
+    cos_theta2=cos_theta*cos_theta
+    sin_theta=np.sin(theta_mid)
+    term1=((k_11-k_33)*cos_theta2+k_33)*d2theta_dz2
+    term2=(k_33-k_11)*sin_theta*cos_theta*dtheta_dz*dtheta_dz
     vd=voltage_distribution2(cell_thickness, theta_vals, voltage, eps_par, eps_perp)
-    field=np.array([(vd[i]+vd[i+1])/(2.0*h) for i in range(len(vd)-1)])
-    term3=(eps_par-eps_perp)*eps0*field*field*ct*st
-    el=term1+term2+term3
-    return el
+    field=np.array([(vd[i]+vd[i+1])/(2.0*grid_spacing) for i in range(len(vd)-1)])
+    term3=(eps_par-eps_perp)*eps0*field*field*cos_theta*sin_theta
+    el_result=term1+term2+term3
+    return el_result
 
 def el_wrapper(theta_cen, params):
+    """Wrapper routine to put the euler_lagrange equations in to the correct format,
+    to be called by "solve" """
     theta_vals=np.array([0 for i in range(len(theta_cen)+2)], float)
     theta_vals[1:-1]=theta_cen
     #theta_vals[0]=tilt1
     #theta_vals[-1]=tilt2
     theta_vals[0]=params[0]
     theta_vals[-1]=params[1]
-    el=euler_lagrange(theta_vals, params[2], params[3], params[4], params[5], params[6], params[7])
-    return el
+    el_result=euler_lagrange(theta_vals, params[2], params[3], params[4], params[5], params[6], params[7])
+    return el_result
 
 def run_it():
+    """ Wrapper for a main routine to provide exemplary output"""
     cell_thickness=10e-6
-    grid_points=21
-    k_11=2e-10 # LC elastic constant
-    k_33=1e-10 # LC elastic constant
-    eps_par=15.0
+    grid_points=101
+    k_11=1e-10 # LC elastic constant
+    k_33=2e-10 # LC elastic constant
+    eps_par=20.0
     eps_perp=5.0
     z_vals=np.linspace(0, cell_thickness, grid_points) # grid_points values
     tilt1=0.0
     tilt2=0.0
-    a=initialise_theta(z_vals, cell_thickness, tilt1, tilt2, np.pi/4.0)
+    input_profile=initialise_theta(z_vals, cell_thickness, tilt1, tilt2, np.pi/4.0)
     #v_range=[20, 15, 10, 9, 8, 7, 6, 5, 4, 3.5, 3]
-    v_range=[5.0, 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3]
+    #v_range=[5.0, 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0, 2.9, 2.8, 2.7, 2.6, 2.5, 0.0]
+    v_range=[5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0]
     profile=np.zeros((grid_points, len(v_range)), float)
     profile[0, :]=tilt1
     profile[grid_points-1, :]=tilt2
     for v_index, voltage in enumerate(v_range):
-        b=root(el_wrapper, a[1:-1], [tilt1, tilt2, cell_thickness, k_33, k_11, eps_par, eps_perp, voltage], method='hybr', options={'factor':0.1})
-        for t_index, theta_val in enumerate(b.x):
+        sol_struct=root(el_wrapper, input_profile[1:-1], [tilt1, tilt2, cell_thickness, k_33, k_11, eps_par, eps_perp, voltage], method='hybr')
+        for t_index, theta_val in enumerate(sol_struct.x):
             profile[t_index+1, v_index]=theta_val
-            a[t_index+1]=theta_val
-    fig, ax = plt.subplots()
-    c=b.x
-    c=np.insert(c, 0, tilt1)
-    c=np.append(c, tilt2)
+            input_profile[t_index+1]=theta_val
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    sol_array=sol_struct.x
+    sol_array=np.insert(sol_array, 0, tilt1)
+    sol_array=np.append(sol_array, tilt2)
+    mid_tilt=profile[grid_points//2, :]
     for index in range(len(v_range)):
-        ax.plot(z_vals, profile[:, index], label=str(v_range[index])+'V')
-    ax.legend()
+        ax1.plot(z_vals, profile[:, index], label=str(v_range[index])+'V')
+    ax1.legend()
+    ax2.plot(v_range, mid_tilt)
+    #ax2.set_xlim([0, 5])
     plt.show()
+
 
 if __name__=='__main__':
     run_it()
---
+++ twisted_nematic.py
@@ -0,0 +1,212 @@
+"""Solve the continuum equation set, describing the relaxation of a twisted
+nematic layer from an initially distorted configuration to a relaxed state in
+the presence of an applied field."""
+
+from math import pi, sin
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import root
+
+eps0=8.854e-12
+
+def initialise_theta(grid_points, tilt1, tilt2, max_tilt):
+    """The routine is used to seed a smoothly tilted director configuration as a starting point for
+    the numerical solution routines, to avoid the trivial solution tilt=0 (for all z) which is
+    valid at all applied fields if tilt1=-tilt2"""
+    thetas=np.array([tilt1+(tilt2-tilt1)*(index/(grid_points-1))+max_tilt*sin(pi*(index/(grid_points-1))) for index in range(grid_points)])
+    return thetas
+
+def initialise_phi(z_vals, twist):
+    """Initialise the phi values to a uniform ramp from 0 to twist"""
+    phis=np.linspace(0.0, twist, z_vals)
+    return phis
+
+def voltage_distribution(cell_thickness, theta_vals, voltage, eps_par, eps_perp):
+    """Given the LC properties and tilt profile, calculate the non-uniform
+    potential distribution across a nematic layer resulting from the LC
+    anisotropy. Interpolate theta values in gaps."""
+
+    # For N grid points, there are N-1 dielectric layers. Interpolate the tilt angles.
+    gap_thetas=[(theta_vals[i]+theta_vals[i+1])/2.0 for i in range(len(theta_vals)-1)]
+    cos_theta=np.cos(gap_thetas) # cos(theta)
+    cos_theta_sq=cos_theta*cos_theta # cos(theta)^2
+    eps=eps_perp*cos_theta_sq+eps_par*(1-cos_theta_sq) # Effective relative permittivity for a tilted sub-layer
+    cap=eps0*eps/(cell_thickness/(len(theta_vals)-1)) # Capacitance of each sub-layer, per unit area
+    total_cap=1/np.sum(1/cap) # Capacitance of the whole stack of layers, in series
+    charge_density=voltage*total_cap # Charge on the stack, per unit area, under potential of voltage
+    potential_drop=charge_density/cap # voltage drop across each sub-layer
+    potential=np.array(theta_vals)
+    potential[0]=0.0
+    for i in range(len(potential_drop)):
+        potential[i+1]=potential[i]+potential_drop[i]
+    potential[-1]=voltage
+    return potential_drop
+
+def voltage_distribution2(cell_thickness, theta_vals, voltage, eps_par, eps_perp):
+    """Given the LC properties and tilt profile, calculate the non-uniform
+    potential distribution across a nematic layer resulting from the LC
+    anisotropy. Interpolate epsilon values in gaps."""
+
+    # For N grid points, there are N-1 dielectric layers. Interpolate the permittivities.
+    #gap_thetas=[(theta_vals[i]+theta_vals[i+1])/2.0 for i in range(len(theta_vals)-1)]
+    cos_theta=np.cos(theta_vals) # cos(theta)
+    cos_theta_sq=cos_theta*cos_theta # cos(theta)^2
+    eps=eps_perp*cos_theta_sq+eps_par*(1-cos_theta_sq) # Effective relative permittivity at each grid point
+    gap_eps=np.array([(eps[i]+eps[i+1])/2.0 for i in range(len(theta_vals)-1)])
+    cap=eps0*gap_eps/(cell_thickness/(len(theta_vals)-1)) # Capacitance of each sub-layer, per unit area
+    total_cap=1/np.sum(1/cap) # Capacitance of the whole stack of layers, in series
+    charge_density=voltage*total_cap # Charge on the stack, per unit area, under potential of voltage
+    potential_drop=charge_density/cap # voltage drop across each sub-layer
+    potential=np.array(theta_vals)
+    potential[0]=0.0
+    for i in range(len(potential_drop)):
+        potential[i+1]=potential[i]+potential_drop[i]
+    potential[-1]=voltage
+    return potential_drop
+
+
+def euler_lagrange(euler_vals, cell_thickness, k_33, k_22, k_11, eps_par, eps_perp, voltage, inv_P):
+    """ The Euler-Lagrange equations are defined in the .wxmx file. This routine takes a vector
+    argument of length 2*grid_points and returns a vector or length 2*grid_points-4; the wrapper
+    function takes care of this and also handles the constant arguments correctly"""
+    
+    # We'll be using a finite difference approximation to the derivative terms dtheta/dz and
+    # d^2theta/dz^2. The equations we need to solve are centred on theta[1] to theta[-2];
+    # theta[0] and theta[-1] are constant boundary values. Approximate the derivatives by central
+    # differences, and to aid this, define the theta ranges (theta_plus, theta_minus) centred on
+    # i+1 and i-1 respectively.
+    base_length=int(len(euler_vals)/2)
+    theta_vals=euler_vals[:base_length]
+    theta_plus=np.array(theta_vals[2:])
+    theta_minus=np.array(theta_vals[:-2])
+    theta_mid=np.array(theta_vals[1:-1])
+    grid_spacing=cell_thickness/(len(theta_vals)-1)
+    dtheta_dz=(theta_plus-theta_minus)/(2*grid_spacing)
+    d2theta_dz2=(theta_plus+theta_minus-2*theta_mid)/(grid_spacing*grid_spacing)
+    
+    phi_vals=euler_vals[base_length:]
+    phi_plus=np.array(phi_vals[2:])
+    phi_minus=np.array(phi_vals[:-2])
+    phi_mid=np.array(phi_vals[1:-1])
+    dphi_dz=(phi_plus-phi_minus)/(2*grid_spacing)
+    d2phi_dz2=(phi_plus+phi_minus-2*phi_mid)/(grid_spacing*grid_spacing)
+
+    # These are vectorized functions, which operate on the whole list of theta values
+    cos_theta=np.cos(theta_mid)
+    cos_theta_sq=cos_theta*cos_theta
+    cos_theta_cu=cos_theta_sq*cos_theta
+    cos_theta_qd=cos_theta_sq*cos_theta_sq
+    sin_theta=np.sin(theta_mid)
+    
+    vd=voltage_distribution(cell_thickness, theta_vals, voltage, eps_par, eps_perp)
+    field=np.array([(vd[i]+vd[i+1])/(2.0*grid_spacing) for i in range(len(vd)-1)])
+    
+    el1_term1=-2*k_33*cos_theta_cu*sin_theta*dphi_dz*dphi_dz
+    el1_term2=2*k_22*cos_theta_cu*sin_theta*dphi_dz*dphi_dz
+    el1_term3=k_33*cos_theta*sin_theta*dphi_dz*dphi_dz
+    el1_term4=-4*np.pi*k_22*cos_theta*sin_theta*dphi_dz*inv_P
+    el1_term5=-k_33*cos_theta_sq*d2theta_dz2
+    el1_term6=k_11*cos_theta_sq*d2theta_dz2
+    el1_term7=k_33*d2theta_dz2
+    el1_term8=k_33*cos_theta*sin_theta*dtheta_dz*dtheta_dz
+    el1_term9=-k_11*cos_theta*sin_theta*dtheta_dz*dtheta_dz
+    el1_term10=-eps_perp*field*field*eps0*cos_theta*sin_theta
+    el1_term11=eps_par*field*field*eps0*cos_theta*sin_theta
+    el1_result=el1_term1+el1_term2+el1_term3+el1_term4+el1_term5+el1_term6+el1_term7+el1_term8+el1_term9+el1_term10+el1_term11
+
+    el2_term1=-k_33*cos_theta_qd*d2phi_dz2
+    el2_term2=k_22*cos_theta_qd*d2phi_dz2
+    el2_term3=k_33*cos_theta_sq*d2phi_dz2
+    el2_term4=4*k_33*cos_theta_cu*sin_theta*dtheta_dz*dphi_dz
+    el2_term5=-4*k_22*cos_theta_cu*sin_theta*dtheta_dz*dphi_dz
+    el2_term6=-2*k_33*cos_theta*sin_theta*dtheta_dz*dphi_dz
+    el2_term7=4*np.pi*k_22*cos_theta*sin_theta*dtheta_dz*inv_P
+    el2_result=el2_term1+el2_term2+el2_term3+el2_term4+el2_term5+el2_term6+el2_term7
+
+    el_result=np.concatenate((el1_result, el2_result)) # String together the theta and phi values
+    return el_result
+
+def el_wrapper(euler_cen, params):
+    """Wrapper routine to put the euler_lagrange equations into the correct format,
+    to be called by "solve"
+    params=[tilt1, tilt2, twist, cell_thickness, k_33, k_22, k_11, eps_par, eps_perp, voltage, inv_P]"""
+    base_length=int(len(euler_cen)/2)
+    theta_vals=np.array([0 for i in range(base_length+2)], float)
+    theta_vals[1:-1]=euler_cen[:base_length]
+    theta_vals[0]=params[0]
+    theta_vals[-1]=params[1]
+
+    phi_vals=np.array([0 for i in range(base_length+2)], float)
+    phi_vals[1:-1]=euler_cen[base_length:]
+    phi_vals[0]=0.0
+    phi_vals[-1]=params[2]
+    euler_vals=np.concatenate((theta_vals, phi_vals))
+    el_result=euler_lagrange(euler_vals, params[3], params[4], params[5], params[6], params[7], params[8], params[9], params[10])
+    return el_result
+
+def run_it():
+    """ Wrapper for a main routine to provide exemplary output"""
+    grid_points=101
+    twist=np.pi/2 # TN cell
+    tilt1=pi/90.0 # 2 degree pretilt
+    tilt2=pi/90.0
+    max_tilt=np.pi/36
+    cell_thickness=10e-6
+    z_vals=np.linspace(0, cell_thickness, grid_points)
+    voltage=6.0
+    k_11=11.7e-12 # LC elastic constants
+    k_22=8.8e-12
+    k_33=19.5e-12
+    eps_par=19.5 # LC permittivities
+    eps_perp=5.17
+    inv_P=1/(300e-6) # Inverse chiral pitch length
+
+    # Define a starting configuration with the correct twist and slight
+    # tilt
+    a=initialise_phi(grid_points, twist)
+    b=initialise_theta(grid_points, tilt1, tilt2, max_tilt)
+    c=np.concatenate((b, a))
+    # The root function accepts a "guess" configuration which excludes
+    # the fixed boundaries - and returns a solution of the same length.
+    # Here is the starting guess.
+    euler_cen=np.concatenate((b[1:-1], a[1:-1]))
+    v_range=[a/100 for a in range(0, 501)] # 0V to 5V in small steps
+    profile=np.zeros((2*grid_points, len(v_range)), float) # An array to put the answers in
+
+    for v_index, voltage in enumerate(v_range):
+        sol_struct=root(el_wrapper, euler_cen, [tilt1, tilt2, twist, cell_thickness, k_33, k_22, k_11, eps_par, eps_perp, voltage, inv_P], method='hybr')
+        # Put the answer into its array, and update the "guess" for each step, to
+        # the solution found at the previous voltage.
+        for t_index, theta_val in enumerate(sol_struct.x[:int(len(euler_cen)/2)]):
+            profile[t_index+1, v_index]=theta_val
+            euler_cen[t_index]=theta_val
+        for t_index, phi_val in enumerate(sol_struct.x[int(len(euler_cen)/2):]):
+            profile[t_index+3+int(len(euler_cen)/2), v_index]=phi_val
+            euler_cen[t_index+int(len(euler_cen)/2)]=phi_val
+    # Put the boundary values into the solution array
+    rows=np.shape(profile)[0]
+    profile[0, :]=tilt1
+    profile[int(rows/2)-1, :]=tilt2
+    profile[int(rows/2), :]=0.0
+    profile[-1, :]=twist
+    # And plot a few things
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+    col_list=[]
+    for index, value in enumerate(v_range):
+        if 2*value==int(2*value):
+            col_list.append(index)
+    for col in col_list:
+        ax1.plot(z_vals, profile[:int(rows/2), col], label=str(v_range[col])+'V')
+        ax2.plot(z_vals, profile[int(rows/2):, col])
+    ax3.plot(v_range[:], profile[int(rows/4), :])
+    ax1.set_ylabel('Through-cell tilt')
+    ax2.set_ylabel('Through-cell twist')
+    ax3.set_xlabel('V')
+    ax3.set_ylabel('Mid-plane tilt')
+    ax1.legend()
+    plt.show()
+
+
+
+if __name__=='__main__':
+    run_it()
